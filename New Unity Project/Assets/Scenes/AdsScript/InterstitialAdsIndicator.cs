a368c20cd997ebe5b399d630d3411bc4be30f6f7
bastien-seira <bastien.seira@epita.fr>
2019-06-28 15:38:50 +0200

    [PART2] Add Ready Indicator for Rewarded Video and Interstitial

---
+++ New Unity Project/Assets/Scenes/AdsScript/InterstitialAdsIndicator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+
+public class InterstitialAdsIndicator : MonoBehaviour, IUnityAdsListener
+{
+
+    string gameId = "3203604";
+    string placementId = "interstitialVideo";
+    bool testMode = true;
+    RawImage image;
+
+    void Start()
+    {
+        image = GetComponent<RawImage>();
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Failed) {
+            Debug.LogWarning("Ads failed to finish because an error occured");
+        }
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        if (placementId == this.placementId)
+        {
+            image.color = Color.green;
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        image.color = Color.red;
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        Debug.LogWarning("Ads dids error " + message);
+    }
+}
